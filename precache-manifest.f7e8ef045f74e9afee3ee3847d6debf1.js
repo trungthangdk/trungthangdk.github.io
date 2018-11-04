ed6f2afb0d422845b0e0f921e26701c879df60e0
trungthangdk <trungthangdk@gmail.com>
2018-11-04 12:51:04 +0700

    set up

---
+++ precache-manifest.f7e8ef045f74e9afee3ee3847d6debf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be5310229730d953c0b5",
+    "url": "/static/js/main.be531022.chunk.js"
+  },
+  {
+    "revision": "a5e2bb9ce2709e18dab3",
+    "url": "/static/js/1.a5e2bb9c.chunk.js"
+  },
+  {
+    "revision": "be5310229730d953c0b5",
+    "url": "/static/css/main.127308ae.chunk.css"
+  },
+  {
+    "revision": "a5e2bb9ce2709e18dab3",
+    "url": "/static/css/1.b12e4298.chunk.css"
+  },
+  {
+    "revision": "8ea47497c6a95d4b10d2d374c2b8188e",
+    "url": "/index.html"
+  }
+];
